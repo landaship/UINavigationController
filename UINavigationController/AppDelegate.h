291bfac059de54bf8243a71db37c1c1b0d305171
landaship <landaship@163.com>
2014-07-09 14:19:34 +0800

    Initial Commit

---
+++ UINavigationController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UINavigationController
+//
+//  Created by Louis on 14-7-9.
+//  Copyright (c) 2014年 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
